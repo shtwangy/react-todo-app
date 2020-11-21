5334bccafd8dcb70308923d81221c041b785e641
shtwangy <shtwangy@gmail.com>
2020-11-22 01:45:17 +0900

    feat(src/App.tsx): input-text state, todos state defined. display todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import {Form} from "./components";
+import {Form, TodoList} from "./components";
+import {Todo} from "./types/todo";
 
 function App() {
-  return (
-    <div className="App">
-        <header>
-            <h1>Shtwangy's ToDo List</h1>
-        </header>
-        <Form/>
-    </div>
-  );
+    const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([] as Todo[]);
+    return (
+        <div className="App">
+            <header>
+                <h1>Shtwangy's ToDo List</h1>
+            </header>
+            <Form
+                todos={todos}
+                setTodos={setTodos}
+                inputText={inputText}
+                setInputText={setInputText}
+            />
+            <TodoList />
+        </div>
+    );
 }
 
 export default App;

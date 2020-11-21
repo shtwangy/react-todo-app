7a49d8241a4d24265545122a7ef8b3b56813ecae
shtwangy <shtwangy@gmail.com>
2020-11-21 21:29:19 +0900

    fix(src/App.tsx): title displayed on header tag

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Shtwangy's ToDo List</h1>
+        <header>
+            <h1>Shtwangy's ToDo List</h1>
+        </header>
     </div>
   );
 }

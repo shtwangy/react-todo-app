ca3e4c07da5a4cc378038398e39d93248ceafc85
shtwangy <shtwangy@gmail.com>
2020-11-21 23:04:32 +0900

    feat(src/components/TodoList): create Todo list component

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+
+interface Props {
+}
+
+const TodoList: FC<Props> = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TodoList;

3fcd447b627c19480170d54913871d9654772bee
shtwangy <shtwangy@gmail.com>
2020-11-22 01:43:25 +0900

    feat(src/Form.tsx): reflect added todo in todos prop.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,15 @@
 import React, {FC} from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {TextInput} from "./UIKit";
+import {SelectBox, TextInput} from "./UIKit";
 import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@material-ui/icons/Add";
+import {Todo} from "../types/todo";
 
 interface Props {
+    todos: Todo[];
+    setTodos: (todos: Todo[]) => void;
+    inputText: string;
+    setInputText: (s: string) => void;
 }
 
 const useStyles = makeStyles(() => ({
@@ -18,6 +23,29 @@ const useStyles = makeStyles(() => ({
 
 const Form: FC<Props> = (props: Props) => {
     const classes = useStyles();
+
+    const inputTextHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        props.setInputText(e.target.value);
+    };
+
+    const submitTodoHandler = () => {
+        console.log('submit');
+        props.setTodos([
+            ...props.todos,
+            {
+                text: props.inputText,
+                completed: false,
+                id: Math.random() * 1000
+            }
+        ]);
+        props.setInputText('');
+    };
+    const filters = [
+        {id: 'all', name: 'ALL'},
+        {id: 'completed', name: 'Completed'},
+        {id: 'uncompleted', name: 'Uncompleted'}
+    ];
     return (
         <div className={classes.form}>
             <TextInput
@@ -26,11 +54,22 @@ const Form: FC<Props> = (props: Props) => {
                 multiline={false}
                 required={false}
                 rows={1}
+                value={props.inputText}
                 type={"text"}
+                onChange={inputTextHandler}
             />
-            <IconButton>
+            <IconButton onClick={submitTodoHandler}>
                 <AddIcon />
             </IconButton>
+            <div>
+                <SelectBox
+                    label={'FILTER'}
+                    required={true}
+                    value={'ALL'}
+                    options={filters}
+                    select={() => {}}
+                />
+            </div>
         </div>
     );
 };

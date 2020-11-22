6854fa7a8c95d1f8fb6aee49ed3dd720e0ba375d
shtwangy <shtwangy@gmail.com>
2020-11-22 16:54:31 +0900

    feat(src/components/TodoList): display todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,21 @@
 import React, {FC} from 'react';
+import {TodoListItem} from "./index";
+import List from "@material-ui/core/List";
+import {Todo} from "../types/todo";
 
 interface Props {
+    todos: Todo[];
 }
 
 const TodoList: FC<Props> = (props: Props) => {
+    const todos = props.todos;
     return (
         <div>
-            
+            <List>
+                {todos.length > 0 && (
+                    todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)
+                )}
+            </List>
         </div>
     );
 };
